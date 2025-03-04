a8334d85b42f4eb71846ec2c5cbd19b1f03474a5
user <User@User.com>
2025-03-04 08:26:59 +0000

    Feature001 - Third Commit, Add Function

--- Source.cpp
+++ Source.cpp
@@ -4,9 +4,14 @@
 //CMake: cmake -G "MinGW Makefiles" -B Build
 //CMake: cmake --build ./Build
 
+int add(int x, int y)
+{
+	return x + y;
+}
+
 int main()
 {
-	std::cout<<"Hello World - First Program Second Commit\n";
-	std::cout<<"The Sum of 1 + 3 = 4\n";
+	std::cout<<"Hello World - First Program Third Commit\n";
+	std::cout<<"The Sum of 1 + 3 = " << add(1,3) << "\n";
 	return 0;
 }
