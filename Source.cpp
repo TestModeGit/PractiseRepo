625e605bcd96776301e76f251c1153285bf405bb
user <User@User.com>
2025-03-04 07:44:01 +0000

    Main - First Commit

---
+++ Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+//Compile: g++ Source.cpp -o Test.exe
+//CMake: cmake -G "MinGW Makefiles" -B Build
+//CMake: cmake --build ./Build
+
+int main()
+{
+	std::cout<<"Hello World - First Program Initial Commit\n";
+	return 0;
+}
